cae983163bca6d52f749ccc08297a88c09573fe6
Joshua Schwartzman <jas9049@psu.edu>
2024-03-03 17:02:35 -0500

    Got sticky working

--- src/alert-message.js
+++ src/alert-message.js
@@ -15,10 +15,17 @@ export class alertMessage extends LitElement {
         if (localStorage.getItem('openStatus') == "false") {
             this.open = false;
         }
+        this.sticky = false;
     }
 
     static get styles() {
         return css`
+
+        :host([sticky]) {
+            position: sticky;
+            top: 0;
+            z-index: 1000;
+        }
 
         .alert-icon {
             width: 50px;
@@ -192,7 +199,7 @@ export class alertMessage extends LitElement {
     render() {
         return html`
         <div class="alert-wrapper">
-            <div class="alert-wrap">
+            <div class="alert-wrap" ?sticky="${this.sticky}">
                 <div class="closed-alert hide-alert">
                     <button class="open-alert" @click=${this.toggleAlert}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="82" height="82" viewBox="0 0 82 82" class="alert-icon"><g transform="translate(-350.099 -428.714)"><g transform="translate(350.099 428.714)" fill="none" stroke-width="6"><circle cx="41" cy="41" r="41" stroke="none"></circle><circle cx="41" cy="41" r="38" fill="none"></circle></g><g transform="translate(384.41 448.566)"><rect width="10.381" height="7.786" transform="translate(0.919 34.336)"></rect><path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)" class="alert-icon-min"></path></g></g></svg>
