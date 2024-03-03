35f7507271e9cbea2a61447d8a26c0003ebe9eee
Joshua Schwartzman <jas9049@psu.edu>
2024-03-03 16:52:24 -0500

    formatting changes

--- src/alert-message.js
+++ src/alert-message.js
@@ -27,18 +27,95 @@ export class alertMessage extends LitElement {
 
         .open-alert {
             border: none;
-            background-color: lightblue;
+            background-color: transparent;
+            cursor: pointer;
+            text-transform: uppercase;
+            font-weight: 700;
+            font-style: italic;
+            display: -webkit-box;
+            display: flex;
+            -webkit-box-pack: center;
+            justify-content: center;
+            -webkit-box-align: center;
+            align-items: center;
+            color: #000321;
+            font-size: 1.25rem;
+            text-align: center;
+            margin: 0em;
+            padding-block: 1px;
+            padding-inline: 6px;
+            letter-spacing: normal;
+            word-spacing: normal;
+            line-height: normal;
         }
 
         .closed-alert {
             display: block;
-            background-color: lightblue;
+            background-color: #bf8226;
+            align-items: center;
+            justify-content: center;
         }
 
         .opened-alert {
             display: none;
-            background-color: blue;
-            color: white;
+            background-color: #bf8226;
+            color: #000321;
+        }
+
+        .close-alert {
+            position: relative;
+            display: block;
+            float: right;
+            top: 0;
+            right: 2.5rem;
+            border: none;
+            background-color: transparent;
+            cursor: pointer;
+            text-transform: uppercase;
+            padding-top: 2rem;
+            padding-bottom: 0.5rem;
+            color: #ffffff;
+            font-size: 1rem;
+            line-height: 1.125rem;
+            font-weight: 700;
+            letter-spacing: 0.03rem;
+        }
+
+        .date {
+            margin-left: 2.5rem;
+            -webkit-box-flex: 1;
+            flex: 1 1 140px;
+            text-transform: uppercase;
+            -webkit-box-ordinal-group: 2;
+            order: 1;
+            align-self: center;
+            font-size: 1rem;
+            line-height: 1.125rem;
+            font-weight: 700;
+            letter-spacing: 0.03rem;
+            color: #ffffff;
+        }
+
+        .message-wrapper {
+            -webkit-box-align: unset;
+            align-items: unset;
+            margin: auto 2.5rem;
+            -webkit-transform: skew(20deg);
+            transform: skew(20deg);
+            -webkit-box-ordinal-group: 4;
+            order: 3;
+            -webkit-box-flex: 1;
+            flex: 1 1 100%;
+            margin: auto 0;
+            display: -webkit-box;
+            display: flex;
+            -webkit-box-orient: horizontal;
+            -webkit-box-direction: normal;
+            flex-direction: row;
+            align-self: flex-end;
+            -webkit-box-align: center;
+            align-items: center;
+            line-height: 1.2;
         }
 
         :host([alertStatus="notice"]) .open-alert {
@@ -123,11 +200,13 @@ export class alertMessage extends LitElement {
                     </button>
                 </div>
                 <div class="opened-alert show-alert">
-                    <slot><p>${this.alertDate}</p></slot>
+                    <slot><p class="date">${this.alertDate}</p></slot>
                     <button class="close-alert" @click=${this.toggleAlert}>Close</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 82" class="alert-icon"><g transform="translate(-350.099 -428.714)"><g transform="translate(350.099 428.714)" fill="none" stroke-width="6"><circle cx="41" cy="41" r="41" stroke="none"></circle><circle cx="41" cy="41" r="38" fill="none"></circle></g><g transform="translate(384.41 448.566)"><rect width="10.381" height="7.786" transform="translate(0.919 34.336)"></rect><path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)"></path></g></g></svg>
-                    <div class="message">
-                        <slot><p>${this.alertParagraph}</p></slot>
+                    <div class="masseage-wrapper">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 82" class="alert-icon"><g transform="translate(-350.099 -428.714)"><g transform="translate(350.099 428.714)" fill="none" stroke-width="6"><circle cx="41" cy="41" r="41" stroke="none"></circle><circle cx="41" cy="41" r="38" fill="none"></circle></g><g transform="translate(384.41 448.566)"><rect width="10.381" height="7.786" transform="translate(0.919 34.336)"></rect><path d="M6520.672,2327.554h-5.854l-3.21-23.669V2299.2h11.81v4.681Z" transform="translate(-6511.607 -2299.203)"></path></g></g></svg>
+                        <div class="message">
+                            <slot><p>${this.alertParagraph}</p></slot>
+                        </div>
                     </div>
                 </div>
             </div>
