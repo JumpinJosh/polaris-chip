3d08d875b6ee0932b9f6ec3b4fbabd1ac0cd9a4c
Joshua Schwartzman <jas9049@psu.edu>
2024-03-21 14:23:18 -0400

    Initial design of UI complete

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -9,36 +9,111 @@ export class HaxcmsPartyUI extends DDD {
 
     constructor() {
         super();
+        this.characters = [];
+        this.userName = "JumpinJosh"
     }
 
     static get styles() {
         return [
             super.styles,
-            css``
+            css`
+            :host {
+                display: block;
+            }
+
+            .ui-wrapper {
+                border-radius: var(--ddd-radius-md);
+                background-color: var(--ddd-theme-default-nittanyNavy);
+                width: auto;
+                height: 250px;
+            }
+
+            .title {
+                color: var(--ddd-theme-default-white);
+                text-align: center;
+            }
+
+            .add-button {
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+                height: fit-content;
+                width: fit-content;
+                color: var(--ddd-theme-default-nittanyNavy);
+                background-color: var(--ddd-theme-default-white);
+            }
+
+            .save-button {
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+                height: fit-content;
+                width: fit-content;
+                color: var(--ddd-theme-default-nittanyNavy);
+                background-color: var(--ddd-theme-default-white);
+            }
+
+            .cancel-button {
+                justify-content: center;
+                align-items: center;
+                cursor: pointer;
+                height: fit-content;
+                width: fit-content;
+                color: var(--ddd-theme-default-nittanyNavy);
+                background-color: var(--ddd-theme-default-white);
+            }
+
+            .input-block {
+                margin: 0;
+                position: absolute;
+                top: 25%;
+                left: 50%;
+                -ms-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+            }
+
+            .save-cancel-wrapper {
+                margin: 0;
+                position: absolute;
+                top: 45%;
+                left: 50%;
+                -ms-transform: translate(-50%, -50%);
+                transform: translate(-50%, -50%);
+                align-content: center;
+            }
+            `
         ];
+    }
+
+    addItem(e) {
+        const characterName = this.shadowRoot.querySelecter(".username-text").value;
+    
+        const character = {
+          name: characterName,
+        }
+        console.log(character);
+        this.characters = [...this.characters, character];
+        console.log(this.characters);
     }
 
     render() {
         return html`
-        <div>
-            <p>Add Members</p>
-            <div>
-                <input type="text" class="Username-Textbox" placeholder="Enter username">
-                <button class="add-button">Add</button>
+        <div class="ui-wrapper">
+            <p class="title">Add Party Members</p>
+            <div class="input-block">
+                <input type="text" class="username-textbox" placeholder="Enter username">
+                <button class="add-button" @click="${this.addItem}">Add</button>
             </div>
-            <div>
-                <rpg-character></rpg-character>
-                <rpg-character></rpg-character>
-                <rpg-character></rpg-character>
-                <rpg-character></rpg-character>
-                <rpg-character></rpg-character>
+            <div class="character-wrapper">
+                <button class="delete-button">Delete</button>
+                ${this.characters.map((character) => html`
+                    <rpg-character seed="${this.userName}"></rpg-character>
+                    <p class="user-name">${this.userName}</p>
+                `)}
             </div>
-            <div>
-                <button class="delete-button">delete</button>
-            </div>
-            <div>
-                <button class="save-button">save</button>
-                <button class="cancel-button">save</button>
+            <div class="save-cancel-wrapper">
+                <button class="save-button">Save</button>
+                <button class="cancel-button">Cancel</button>
             </div>
         </div>
         `
@@ -46,7 +121,7 @@ export class HaxcmsPartyUI extends DDD {
 
     static get properties() {
         return {
-
+            characters: { type: Array },
         }
     }
 }
